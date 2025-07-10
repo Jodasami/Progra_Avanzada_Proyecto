4a172ff42b75cd553f09df5ee032bc36c4d3ef3b
Jodasami <82111265+Jodasami@users.noreply.github.com>
2025-07-10 09:15:54 -0600

    1 Avance del proyecto 50%
    Se realizó el modulo de Usuarios al 100%, falta algun módulo extra para poder cumplir con la entrega

---
+++ VentaMusical/VentaMusical/Controllers/VentasController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VentaMusical.Controllers
+{
+    public class VentasController : Controller
+    {
+        // GET: Ventas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Ventas/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Ventas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ventas/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
